3950186e4c06efcd9776dddaef2c082496f367f9
PatrickVuscan <patrick.vuscan@gmail.com>
2021-10-23 10:30:08 -0400

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+// import Navbar from './components/Navbar';
+import theme from './mui';
+
+// This is the entry into the app, so we have this bad boy making sure we import certain files
+// only when we're in the development mode
+if (process.env.NODE_ENV === 'development') {
+  import('./random').then(() => console.log('loaded in random'));
+}
 
 function App() {
-  return <div className="app" />;
+  console.log(process.env);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="app">
+          {/* <Navbar /> */}
+          <Switch>
+            <Route path="/abc">{window.largeLorem}</Route>
+            <Route path="/xyz">{window.largeLorem}</Route>
+            <Route path="/">You are on the home page</Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
