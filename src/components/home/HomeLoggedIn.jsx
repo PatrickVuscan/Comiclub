60e7a0c78c901fd3ba21bf45d9d1036344fdc710
rkash <raag.kashyap@mail.utoronto.ca>
2021-10-29 03:07:46 -0400

    Adjusted row size to have images display properly

--- src/components/home/HomeLoggedIn.jsx
+++ src/components/home/HomeLoggedIn.jsx
@@ -67,7 +67,7 @@ const HomeLoggedIn = () => {
           }}
         >{`Welcome, ${user}! Here are some of your Subscriptions`}</p>
 
-        <ImageList sx={{ width: 800, height: 900 }} cols={3} rowHeight={400}>
+        <ImageList sx={{ width: 800, height: 900 }} cols={3} rowHeight={440}>
           {itemData.map((item) => (
             <ImageListItem key={item.img}>
               <img
