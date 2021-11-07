9468e5d60d05182dbec196f40bf3ff66e17065b5
salmanshah1d <s.shahid@mail.utoronto.ca>
2021-11-07 12:54:25 -0500

    removed unnecessary image, set comic cover based on comic, likes functionality

--- src/components/comics/ComicsSummary.jsx
+++ src/components/comics/ComicsSummary.jsx
@@ -1,42 +1,59 @@
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import comicCoverImg from '../../assets/comicCover.jpg';
 import formatMetric from '../../utilities';
+import aotImage from '../home/images/aot.png';
+import batmanImage from '../home/images/batman.png';
+import ironman1Image from '../home/images/ironman.png';
+import marvelImage from '../home/images/marvel.png';
+import narutoImage from '../home/images/naruto.png';
+import opImage from '../home/images/op.png';
+import togImage from '../home/images/tog.png';
 import ComicEpisodeCard from './ComicEpisodeCard';
 import styles from './ComicsSummary.module.css';
 
 function fetchComicFromDB(comicId) {
   const comicDB = {
     ironman1: 'Ironman: The First Edition',
-    ironman2: 'Ironman: The Second Edition',
-    tokyo: 'Tokyo Ghoul',
-    deathnote: 'Death Note',
     aot: 'Attack on Titan',
     batman: 'Batman Origins',
     marvel: 'Marvel',
     naruto: 'Naruto',
     op: 'One Piece',
     tog: 'Tower of God',
-    goh: 'God of High School',
+  };
+
+  const comicPhotoDB = {
+    ironman1: ironman1Image,
+    aot: aotImage,
+    batman: batmanImage,
+    marvel: marvelImage,
+    naruto: narutoImage,
+    op: opImage,
+    tog: togImage,
   };
 
   const episodes = ['a', 'b', 'c', 'd', 'a', 'b', 'c', 'd', 'a', 'b', 'c', 'd'];
   return {
     comicName: comicDB[comicId],
-    comicCover: comicCoverImg,
+    comicCover: comicPhotoDB[comicId],
     episodes,
     numEpisodes: episodes.length,
     views: 100364,
     subs: 42124,
+    likes: 10,
     description: window.smallLorem,
   };
 }
 
 const ComicsSummary = () => {
   const { comicId } = useParams();
-  const { comicName, comicCover, episodes, numEpisodes, views, subs, description } = fetchComicFromDB(comicId);
+  const { comicName, comicCover, episodes, numEpisodes, views, subs, likes, description } = fetchComicFromDB(comicId);
+
+  const [isLiked, setIsLiked] = useState(false);
 
   return (
     <div>
@@ -49,11 +66,22 @@ const ComicsSummary = () => {
               <p>
                 {formatMetric(numEpisodes, 'Episode')} <br />
                 {formatMetric(views, 'View')} <br />
-                {formatMetric(subs, 'Subscriber')}
+                {formatMetric(subs, 'Subscriber')} <br />
+                {formatMetric(isLiked ? likes + 1 : likes, 'Like')}
               </p>
             </div>
-            <IconButton sx={{ color: 'black' }} aria-label={`heart ${comicName}`}>
-              <FavoriteBorderIcon style={{ fontSize: '50px' }} />
+            <IconButton
+              sx={{ color: 'black' }}
+              aria-label={`heart ${comicName}`}
+              onClick={() => {
+                setIsLiked(!isLiked);
+              }}
+            >
+              {isLiked ? (
+                <FavoriteIcon style={{ fontSize: '50px', color: 'red' }} />
+              ) : (
+                <FavoriteBorderIcon style={{ fontSize: '50px' }} />
+              )}
             </IconButton>
           </div>
           <div className={styles.description}>
