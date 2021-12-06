ec2a6c6cece0667150fe7bb3bca3964eb9b90a01
rkash <raag.kashyap@mail.utoronto.ca>
2021-12-05 23:32:31 -0500

    Added function to get liked comic IDs for a user

--- frontend/src/actions/HomeLoggedInActions.jsx
+++ frontend/src/actions/HomeLoggedInActions.jsx
@@ -22,6 +22,31 @@ export const getUser = async (email) => {
   };
 };
 
+export const getLikedComicID = async (username) => {
+  console.log(`Getting the User by email ${username}`);
+
+  const userResponse = await fetch(`${ENV.api_host}/api/users/${username}/likes`, {
+    credentials: 'include',
+  });
+
+  if (!userResponse.ok) {
+    console.log('There was an error retrieving the user', userResponse.error);
+    return;
+  }
+
+  const userJSON = await userResponse.json();
+
+  console.log('The JSON');
+  console.log(userJSON);
+
+  const { likes } = userJSON;
+
+  return {
+    likes,
+  };
+};
+
 export default {
   getUser,
+  getLikedComicID,
 };
