8ba2744f14134c5be9954b1f9c12ccde2f97e4e0
PatrickVuscan <patrick.vuscan@gmail.com>
2021-12-06 23:16:56 -0500

    Updated Comic Page layout

--- frontend/src/components/comics/ComicsSummary.jsx
+++ frontend/src/components/comics/ComicsSummary.jsx
@@ -42,40 +42,42 @@ const ComicsSummary = () => {
         <div className={styles.summary}>
           <div className={styles.comicSnapshot}>
             <div className={styles.metadata}>
-              <h2 className={styles.headerMargin}>{comic.name ? comic.name.toUpperCase() : 'Comic'}</h2>
-              <p>
+              <div style={{ display: 'flex' }}>
+                <h2 className={styles.headerMargin}>{comic.name ? comic.name.toUpperCase() : 'Comic'}</h2>
+                <IconButton
+                  sx={{ color: 'black' }}
+                  aria-label={`heart ${comic.name}`}
+                  onClick={() => {
+                    setIsLiked(!isLiked);
+                    const func = isLiked ? unlikeComic : likeComic;
+                    const prefix = isLiked ? '' : 'un';
+                    func(comicID)
+                      .then((res) => {
+                        if (res) {
+                          setComic(res);
+                        } else {
+                          alert(`This comic could not be ${prefix}liked.`);
+                        }
+                      })
+                      .catch((err) => console.log(err));
+                  }}
+                >
+                  {isLiked ? (
+                    <FavoriteIcon style={{ fontSize: '50px', color: 'red' }} />
+                  ) : (
+                    <FavoriteBorderIcon style={{ fontSize: '50px' }} />
+                  )}
+                </IconButton>
+              </div>
+              <div className={styles.description}>
+                <p>{comic.description}</p>
+              </div>
+              <p style={{ marginTop: 0 }}>
                 {formatMetric(comic.episodeCount, 'Episode')} <br />
                 {formatMetric(comic.viewCount, 'View')} <br />
                 {formatMetric(comic.likeCount, 'Like')}
               </p>
             </div>
-            <IconButton
-              sx={{ color: 'black' }}
-              aria-label={`heart ${comic.name}`}
-              onClick={() => {
-                setIsLiked(!isLiked);
-                const func = isLiked ? unlikeComic : likeComic;
-                const prefix = isLiked ? '' : 'un';
-                func(comicID)
-                  .then((res) => {
-                    if (res) {
-                      setComic(res);
-                    } else {
-                      alert(`This comic could not be ${prefix}liked.`);
-                    }
-                  })
-                  .catch((err) => console.log(err));
-              }}
-            >
-              {isLiked ? (
-                <FavoriteIcon style={{ fontSize: '50px', color: 'red' }} />
-              ) : (
-                <FavoriteBorderIcon style={{ fontSize: '50px' }} />
-              )}
-            </IconButton>
-          </div>
-          <div className={styles.description}>
-            <p>{comic.description}</p>
           </div>
         </div>
         <h2 className={styles.headerMargin}>EPISODES</h2>
